9f3835a11dbaf79f06027ab2cbefcb5c2b2cd85f
VeberAL <aleo83@rambler.ru>
2021-11-22 23:42:20 +0300

    Комментарии к http-контроллеру

--- src/main/java/com/main/BookController.java
+++ src/main/java/com/main/BookController.java
@@ -4,29 +4,31 @@ import com.main.mapper.BookToDtoMapper;
 import com.main.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController()
-@RequestMapping("/books")
+@RequestMapping("/books") //контроллер будет обрабатывать HTTP-запросы
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
     private final BookToDtoMapper mapper;
+
     //Получение книги по идентификатору
-    @GetMapping("/{id}")
+    @GetMapping("/{id}") //Добавление в метод get с передаваемым id
     public Book getBookById(@PathVariable Long id) {
+                            //извлекает шаблонную часть url
         return bookService.getBookById(id);
     }
     //Получение списка всех книг
-    @GetMapping
+    @GetMapping //Добавление в метод get
     public List<Book> getAllBooks() {
         return bookService.getAllBooks();
     }
     //Добавление книги
-    @PostMapping
+    @PostMapping //Добавление в метод post
     public void addBook(@RequestBody BookRequest request) {
+                       //преобразовывает содержимое тела входящего запроса в объект
         bookService.addBook(mapper.AddBookRequestToBook(request));
     }
 }
