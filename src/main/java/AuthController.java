c0a7efe65604607db6e44ef9302fb0d591fbc123
VeberAL <aleo83@rambler.ru>
2021-12-07 01:09:50 +0300

    Набросок Контроллера авторизации

---
+++ src/main/java/AuthController.java
@@ -0,0 +1,17 @@
+import com.main.model.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    @PostMapping
+    public ResponseEntity<String> register(@RequestBody User user) {
+        return ResponseEntity.ok("Registered");
+    }
+}
